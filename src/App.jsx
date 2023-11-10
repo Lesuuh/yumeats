f20ed084bd023c95c2f096452cb01dcc054d87d4
Lesuuh <lesuuh01@gmail.com>
2023-11-10 17:02:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Delivery from "./components/Delivery"
+import Featured from "./components/Featured"
+import TopNav from "./components/TopNav"
+
+
+function App() {
+
+
+  return (
+    <>
+      <TopNav />
+      <Featured/>
+      <Delivery/>
+    </>
+  )
+}
+
+export default App
